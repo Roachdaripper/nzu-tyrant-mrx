fbb5beb85704a52e89a578bfd32180a36080cc92
Roach <roachdaripper1@gmail.com>
2019-05-12 19:22:47 +0100

    Update settings.lua

--- lua/nzombies-unlimited/extensions/nzu_tyrant_mrx_special_enemy/settings.lua
+++ lua/nzombies-unlimited/extensions/nzu_tyrant_mrx_special_enemy/settings.lua
@@ -9,3 +9,34 @@ local settings = {
 		Default = 2,
 	}
 }
+
+if CLIENT then
+	-- Pre-register these as their files are located in the gamemode, not appearing in sandbox
+
+	local labelledsettings = {
+		{"Health", "Starting Health"},
+		{"NumRounds", "Rounds to stay down for"}
+	}
+
+	local panelfunc = function(p, SettingPanel)
+		for k,v in pairs(labelledsettings) do
+			local pnl = vgui.Create("Panel", p)
+			pnl:SetTall(25)
+			pnl:Dock(TOP)
+			pnl:DockPadding(5,2,5,2)
+
+			local lbl = pnl:Add("DLabel")
+			lbl:SetText(v[2])
+			lbl:Dock(LEFT)
+			lbl:SetWide(125)
+			lbl:DockMargin(0,0,10,0)
+
+			local st = SettingPanel(v[1],pnl)
+			st:Dock(FILL)
+		end
+		return p
+	end
+	return settings, panelfunc
+end
+
+return settings
